e5b16ee80f24359044de38d59cd63efb2d585bd9
Madelaine Caro <madelaine.caro@sistemas.edu.bo>
2024-06-14 02:49:26 -0400

    menu

---
+++ Sistema_DAS/Sistema_DAS/Controllers/MenuController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Sistema_DAS.Controllers
+{
+    public class MenuController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
